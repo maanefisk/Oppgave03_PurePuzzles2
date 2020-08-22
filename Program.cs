8abfddf75b7e0a816897e40785b471da6b1b5b04
maanefisk <62015771+maanefisk@users.noreply.github.com>
2020-08-22 12:41:15 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Oppgave03_PurePuzzles2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
